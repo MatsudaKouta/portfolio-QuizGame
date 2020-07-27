de1c53abb7d9a02796cf508df9f62fd589350385
MatsudaKouta <kou8722@gmail.com>
2020-07-27 22:56:37 +0900

    correct時　画像挿入(仮)

--- main.js
+++ main.js
@@ -16,34 +16,38 @@
   function shuffle(arr) {
     for (let i = arr.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-        [arr[j], arr[i]] = [arr[i], arr[j]];
+      [arr[j], arr[i]] = [arr[i], arr[j]];
     }
     return arr;
   }
 
   function checkAnswer(li) {
-    if (li.textContent === quizSet[currentNum].c[0]){
-        console.log('correct')
+    if (li.textContent === quizSet[currentNum].c[0]) {
+      li.textContent = "OK";
+      li.classList.add("correct");
+      const img = document.getElementById('img');
+      img.src = 'img/correct.jpg'
+       
     } else {
-        console.log('wrong')
+      li.textContent = "Oh.No...";
+      li.classList.add("wrong");
     }
   }
-
 
   function setQuiz() {
     question.textContent = quizSet[currentNum].q;
 
     //選択肢の表示
     const shuffleChoices = shuffle([...quizSet[currentNum].c]);
-    shuffleChoices.forEach(choice => {
+    shuffleChoices.forEach((choice) => {
       const li = document.createElement("li");
       li.textContent = choice;
-      li.addEventListener('click',() => {
-        checkAnswer(li)
-      })
+      li.addEventListener("click", () => {
+        checkAnswer(li);
+      });
       choices.appendChild(li);
     });
   }
 
-  setQuiz()
+  setQuiz();
 }
