e4b6f592461e6668df0fbe0f5751a93f7a3cff32
MatsudaKouta <kou8722@gmail.com>
2020-08-11 22:53:14 +0900

    Initial Commit

--- main.js
+++ main.js
@@ -3,14 +3,24 @@
   const question = document.getElementById("question");
   const choices = document.getElementById("choices");
   const btn = document.getElementById("btn");
+  const img = document.getElementById("img");
+  const score = document.querySelector("#result > p")
+  console.log(score)
 
   const quizSet = [
-    { q: "Aはなにか?", c: ["A0", "A1", "A2"] },
-    { q: "Bはなにか?", c: ["B0", "B1", "B2"] },
-    { q: "Cはなにか?", c: ["C0", "C1", "C2"] },
+    { q: "2002年、木村拓哉と明石家さんま共演作は？", c: ["空から降る一億の星", "GOOD　LUCK！", "HERO"] },
+    { q: "豊川悦司が小説家を演じた作品は?", c: ["Love Story", "愛していると言ってくれ", "青い鳥"] },
+    { q: "大河ドラマ『新選組！』の脚本家は?", c: ["三谷幸喜", "宮藤官九郎", "野島伸司"] },
   ];
 
+  //現在何問目か
   let currentNum = 0;
+  //正答数
+  let correct = 0;
+  //誤答数
+  let wrong = 0;
+  //回答したかどうか
+  let answered = false;
 
   //回答をシャッフルする関数()　フィッシャー・イェーツ
   function shuffle(arr) {
@@ -23,19 +33,29 @@
 
   function checkAnswer(li) {
     if (li.textContent === quizSet[currentNum].c[0]) {
-      li.textContent = "OK";
+      li.textContent = "正解！";
       li.classList.add("correct");
-      const img = document.getElementById('img');
-      img.src = 'img/correct.jpg'
-       
+      img.src = "img/correct.jpg";
+      correct++;
+      // console.log(`正答率${correct}　/ ${quizSet.length} 　誤答数${wrong}`);
     } else {
-      li.textContent = "Oh.No...";
+      li.textContent = "残念...";
       li.classList.add("wrong");
+      wrong++;
+      console.log(wrong);
     }
   }
 
+  //質問と回答　セット関数
   function setQuiz() {
+    answered = false;
+
     question.textContent = quizSet[currentNum].q;
+
+    while (choices.firstChild) {
+      choices.removeChild(choices.firstChild);
+    }
+    img.src = "";
 
     //選択肢の表示
     const shuffleChoices = shuffle([...quizSet[currentNum].c]);
@@ -43,6 +63,13 @@
       const li = document.createElement("li");
       li.textContent = choice;
       li.addEventListener("click", () => {
+        if (answered === true) {
+          return;
+        }
+        answered = true;
+        if (currentNum === quizSet.length - 1){
+          btn.textContent = '結果！'
+        }
         checkAnswer(li);
       });
       choices.appendChild(li);
@@ -50,4 +77,20 @@
   }
 
   setQuiz();
+
+//Nextボタンを押したときの処理
+  btn.addEventListener("click", () => {
+    if (answered === false){
+      alert('回答を選んでね！')
+      return;
+    }
+    answered = false;
+    if (currentNum === quizSet.length - 1) {
+      score.textContent = `正答率${correct}/${quizSet.length} 　誤答数${wrong}`;
+      result.classList.remove('hidden');
+    } else {
+      currentNum++;
+      setQuiz();
+    }
+  });
 }
